3cbbd04c9832bde9541b1bf117f1bffcf441bac7
pkpk8 <swat521@qq.com>
2014-07-03 15:18:28 +0800

    测试

--- gen/com/qq/test_login/R.java
+++ gen/com/qq/test_login/R.java
@@ -29,7 +29,7 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int fragment_main=0x7f030001;
+        public static final int login=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f060000;
